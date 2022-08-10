666177853bd88d7db003f8b7cf085d230c0e739c
Kristoff <chrystalio.k@outlook.com>
2022-08-10 23:49:59 +0700

    Add migration

---
+++ database/migrations/2022_08_10_234730_create_social_accounts_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up()
+    {
+        Schema::create('social_accounts', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id');
+            $table->string('provider_id')->unique();
+            $table->string('provider_name');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('social_accounts');
+    }
+};
